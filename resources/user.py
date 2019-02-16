9408dbc56ca5eed23b1bd96e0ca172b51c5b1dfd
Onkar <onkarmadli@yahoo.in>
2019-02-16 21:16:05 +0530

    Add messages for invalid credentials

--- resources/user.py
+++ resources/user.py
@@ -10,80 +10,80 @@ class UserRegister(Resource):
 
 	parser.add_argument('FirstName',
 		type=str,
-		required=True,
+		required=False,
 		help="FirstName cannot be blank"
 	)
 
 	parser.add_argument('LastName',
 		type=str,
-		required=True,
+		required=False,
 		help="LastName cannot be blank"
 	)
 
-	
+
 	parser.add_argument('EmailId',
 		type=str,
 		required=True,
 		help="Email cannot be blank"
 	)
-	
+
 	parser.add_argument('City',
 		type=str,
-		required=True,
+		required=False,
 		help="City cannot be blank"
 	)
 
 	parser.add_argument('Phone',
 		type=str,
-		required=True,
+		required=False,
 		help="phone cannot be blank"
 	)
 
 	parser.add_argument('DoB',
 		type=str,
-		required=True,
+		required=False,
 		help="Dobl cannot be blank"
 	)
 
 	parser.add_argument('UserId',
 		type=str,
-		required=True,
+		required=False,
 		help="UserId cannot be blank"
 	)
 
 	parser.add_argument('MembershipType',
 		type=str,
-		required=True,
+		required=False,
 		help="Email cannot be blank"
 	)
-	
+
 	parser.add_argument('MembershipSince',
 		type=str,
-		required=True,
+		required=False,
 		help="MembershipSince cannot be blank"
 	)
 
 	parser.add_argument('MembershipTill',
 		type=str,
-		required=True,
+		required=False,
 		help="MembershipTill cannot be blank"
 	)
 
 	parser.add_argument('Rank',
 		type=str,
-		required=True,
+		required=False,
 		help="Rank cannot be blank"
 	)
 
 	parser.add_argument('Role',
 		type=str,
-		required=True,
+		required=False,
 		help="Role cannot be blank"
 	)
 
 	parser.add_argument('Points',
 		type=str,
-		required=True,
+		required=False,
 		help="Email cannot be blank"
 	)
 	parser.add_argument('Password',
@@ -96,19 +96,21 @@ class UserRegister(Resource):
 		data = UserRegister.parser.parse_args()
 
 		if UserModel.find_by_email(data['EmailId']):
-			return {"message": "A user with that username already exists"}, 400
+			return {"message": "Email already exists"}, 400
 
-		user = UserModel(data['FirstName'], data['LastName'], data['EmailId'], data['City'], data['Phone'], data['DoB'], data['UserId'], data['Password'], data['MembershipType'], data['MembershipSince'], data['MembershipTill'], data['Rank'], data['Role'], data['Points'])
+		user = UserModel(EmailId = data['EmailId'], Password = data['Password'])
 		user.save_to_db()
 		return {"message": "User created successfully."}, 201
 
 
 class User(Resource):
-	def get(self,EmailId):
+	def get(self):
 		data = request.data
 		dataDict = json.loads(data)
 		usa = UserModel.find_by_email(dataDict['EmailId'])
-		if usa and check_password_hash(usa.Password, dataDict['Password']):
-			return usa.json(), 200
-		return {'message': 'Email not registered',
-			'data': dataDict}, 404
+		if usa:
+			if check_password_hash(usa.Password, dataDict['Password']):
+				return usa.json(), 200
+			else:
+				return {'message': 'Incorrect username or password'}, 401
+		return {'message': 'Email or username not registered'}, 404
